73ad5bfc829d92ac82d1bfd380849830d1a0912f
lethanhkhoi <lethanhkhoi081001@gmail.com>
2022-07-17 12:45:00 +0700

    update datapagination function

--- helperFunction/helper.js
+++ helperFunction/helper.js
@@ -3,11 +3,12 @@ function dataPagination(match, sort, page = 1, limit = 10) {
   const aggregate = [{ $match: match }];
   let data = [];
   data.push({ $sort: sort });
+
   if (page > 1) {
     let skip = (page - 1) * limit;
     data.push({ $skip: skip });
   }
-  data.push({ $limit: limit });
+  data.push({ $limit: parseInt(limit) });
   aggregate.push({ $facet: { data } });
   return aggregate;
 }
