4c5a0783ab35789018fa7b8903398d1d203842da
lethanhkhoi <lethanhkhoi081001@gmail.com>
2022-08-19 23:52:39 +0700

    try catch error

--- controller/user.js
+++ controller/user.js
@@ -9,11 +9,15 @@ const cartCol = require("../dataModel/cartCol");
 const saltRounds = 10;
 
 async function getAllAdmin(req, res) {
-  const data = await userCol.getAllAdmin();
-  if (!data) {
+  try {
+    const data = await userCol.getAllAdmin();
+    if (!data) {
+      return res.json({ errorCode: true, data: "system error" });
+    }
+    return res.json({ errorCode: true, data });
+  } catch (error) {
     return res.json({ errorCode: true, data: "system error" });
   }
-  return res.json({ errorCode: true, data });
 }
 async function getAll(req, res) {
   const data = await userCol.getAll();
@@ -44,151 +48,171 @@ async function login(req, res) {
   }
 }
 async function register(req, res) {
-  const validation = req.body;
-  for (property of userCol.validation) {
-    if (validation[property] === null) {
-      return res.json({ errorCode: true, data: `Lack of ${property}` });
+  try {
+    const validation = req.body;
+    for (property of userCol.validation) {
+      if (validation[property] === null) {
+        return res.json({ errorCode: true, data: `Lack of ${property}` });
+      }
     }
+    const user = await database.userModel().findOne({ email: req.body.email });
+    if (user) {
+      return res.json({ errorCode: true, data: "Tai khoan da ton tai" });
+    }
+    const checkPass = req.body.password == req.body.confirmPassword;
+    if (!checkPass) {
+      return res.json({ errorCode: true, data: "Confirm password sai" });
+    }
+    const password = await bcrypt.hash(req.body.password, saltRounds);
+    const data = {
+      id: ObjectID().toString(),
+      email: req.body.email,
+      prevPassword: null,
+      password: password,
+      name: req.body.name ?? "",
+      phone: req.body.phone ?? "",
+      address: req.body.address ?? "",
+      gender: req.body.gender ?? "male",
+      birthday: req.body.birthday
+        ? moment(req.body.birthday, "DD/MM/YYYY").utc().toDate()
+        : null,
+      voucher: [],
+      role: req.body.role ?? "user",
+      createdAt: new Date(),
+    };
+    await userCol.create(data);
+    await cartCol.create({
+      id: ObjectID().toString(),
+      userId: data.id,
+      product: [],
+    });
+    if (!data.token) {
+      data.token = await jwt.createSecretKey(req.body.email);
+    }
+    return res.json({ errorCode: null, data: data });
+  } catch (error) {
+    return res.json({ errorCode: true, data: "system error" });
   }
-  const user = await database.userModel().findOne({ email: req.body.email });
-  if (user) {
-    return res.json({ errorCode: true, data: "Tai khoan da ton tai" });
-  }
-  const checkPass = req.body.password == req.body.confirmPassword;
-  if (!checkPass) {
-    return res.json({ errorCode: true, data: "Confirm password sai" });
-  }
-  const password = await bcrypt.hash(req.body.password, saltRounds);
-  const data = {
-    id: ObjectID().toString(),
-    email: req.body.email,
-    prevPassword: null,
-    password: password,
-    name: req.body.name ?? "",
-    phone: req.body.phone ?? "",
-    address: req.body.address ?? "",
-    gender: req.body.gender ?? "male",
-    birthday: req.body.birthday
-      ? moment(req.body.birthday, "DD/MM/YYYY").utc().toDate()
-      : null,
-    voucher: [],
-    role: req.body.role ?? "user",
-    createdAt: new Date(),
-  };
-  await userCol.create(data);
-  await cartCol.create({
-    id: ObjectID().toString(),
-    userId: data.id,
-    product: [],
-  });
-  if (!data.token) {
-    data.token = await jwt.createSecretKey(req.body.email);
-  }
-  return res.json({ errorCode: null, data: data });
 }
 async function update(req, res) {
-  const email = req.params.code;
-  let data = req.body;
-  const user = await userCol.getDetailByEmail(email);
-  if (!user) {
-    return res.json({ errorCode: true, data: "Cannot found this account" });
-  }
-  if (req.body.password) {
-    const checkPass = await bcrypt.compare(data.password, user.password);
-    if (!checkPass) {
-      return res.json({ errorCode: true, data: "Wrong password" });
+  try {
+    const email = req.params.code;
+    let data = req.body;
+    const user = await userCol.getDetailByEmail(email);
+    if (!user) {
+      return res.json({ errorCode: true, data: "Cannot found this account" });
     }
-    data.password = await bcrypt.hash(req.body.password, saltRounds);
-  }
-  if (data.birthday) {
-    data.birthday = req.body.birthday
-      ? moment(data.birthday, "DD/MM/YYYY").utc().toDate()
-      : null;
-  }
-  const update = await userCol.update(email, data);
-  if (!update) {
-    return res.json({ errorCode: true, data: "Update fail" });
-  }
-  for (property of userCol.userProperties) {
-    if (req.body[property]) {
-      update[property] = req.body[property];
+    if (req.body.password) {
+      const checkPass = await bcrypt.compare(data.password, user.password);
+      if (!checkPass) {
+        return res.json({ errorCode: true, data: "Wrong password" });
+      }
+      data.password = await bcrypt.hash(req.body.password, saltRounds);
     }
+    if (data.birthday) {
+      data.birthday = req.body.birthday
+        ? moment(data.birthday, "DD/MM/YYYY").utc().toDate()
+        : null;
+    }
+    const update = await userCol.update(email, data);
+    if (!update) {
+      return res.json({ errorCode: true, data: "Update fail" });
+    }
+    for (property of userCol.userProperties) {
+      if (req.body[property]) {
+        update[property] = req.body[property];
+      }
+    }
+    return res.json({ errorCode: null, data: update });
+  } catch (error) {
+    return res.json({ errorCode: true, data: "system error" });
   }
-  return res.json({ errorCode: null, data: update });
 }
 async function deleteAccount(req, res) {
-  const id = req.params.code;
-  const user = await userCol.getDetailByCode(id);
-  if (!user) {
-    return res.json({ errorCode: true, data: "Khong tim thay account nay" });
-  }
-  if (user.deletedAt) {
-    return res.json({
-      errorCode: true,
-      data: "Account nay da bi ban truoc do",
-    });
-  }
-  const update = await userCol.destroy(id);
-  if (!update) {
-    return res.json({ errorCode: true, data: "System error" });
-  }
-  for (property of userCol.userProperties) {
-    if (req.body[property]) {
-      update[property] = req.body[property];
+  try {
+    const id = req.params.code;
+    const user = await userCol.getDetailByCode(id);
+    if (!user) {
+      return res.json({ errorCode: true, data: "Khong tim thay account nay" });
     }
+    if (user.deletedAt) {
+      return res.json({
+        errorCode: true,
+        data: "Account nay da bi ban truoc do",
+      });
+    }
+    const update = await userCol.destroy(id);
+    if (!update) {
+      return res.json({ errorCode: true, data: "System error" });
+    }
+    for (property of userCol.userProperties) {
+      if (req.body[property]) {
+        update[property] = req.body[property];
+      }
+    }
+    return res.json({ errorCode: null, data: "Ban account thanh cong" });
+  } catch (error) {
+    return res.json({ errorCode: true, data: "system error" });
   }
-  return res.json({ errorCode: null, data: "Ban account thanh cong" });
 }
 
 async function unban(req, res) {
-  const id = req.params.code;
-  const user = await userCol.getDetailByCode(id);
-  if (!user) {
-    return res.json({ errorCode: true, data: "Khong tim thay account nay" });
-  }
-  if (!user.deletedAt) {
-    return res.json({
-      errorCode: true,
-      data: "Account nay chua bi ban truoc do",
-    });
-  }
-  const update = await userCol.unban(id);
-  if (!update) {
-    return res.json({ errorCode: true, data: "System error" });
-  }
-  for (property of userCol.userProperties) {
-    if (req.body[property]) {
-      update[property] = req.body[property];
+  try {
+    const id = req.params.code;
+    const user = await userCol.getDetailByCode(id);
+    if (!user) {
+      return res.json({ errorCode: true, data: "Khong tim thay account nay" });
     }
+    if (!user.deletedAt) {
+      return res.json({
+        errorCode: true,
+        data: "Account nay chua bi ban truoc do",
+      });
+    }
+    const update = await userCol.unban(id);
+    if (!update) {
+      return res.json({ errorCode: true, data: "System error" });
+    }
+    for (property of userCol.userProperties) {
+      if (req.body[property]) {
+        update[property] = req.body[property];
+      }
+    }
+    return res.json({ errorCode: null, data: "Unban account thanh cong" });
+  } catch (error) {
+    return res.json({ errorCode: true, data: "system error" });
   }
-  return res.json({ errorCode: null, data: "Unban account thanh cong" });
 }
 
 async function forgotPassword(req, res) {
-  let data = req.body;
-  if (!data.email) {
-    return res.json({
-      errorCode: true,
-      data: "Please input your account's email",
-    });
-  }
-  const user = await userCol.getDetailByEmail(data.email);
-  if (!user) {
-    return res.json({ errorCode: true, data: "This account is not exist" });
-  }
-  const newpass = Math.floor(Math.random() * 10000) + 1000;
-  data.password = bcrypt.hash(newpass, saltRounds);
-  const update = await userCol.update(user.code, data);
-  if (!update) {
-    return res.json({ errorCode: true, data: "System error" });
-  }
-  for (property of userCol.userProperties) {
-    if (req.body[property]) {
-      update[property] = req.body[property];
+  try {
+    let data = req.body;
+    if (!data.email) {
+      return res.json({
+        errorCode: true,
+        data: "Please input your account's email",
+      });
     }
+    const user = await userCol.getDetailByEmail(data.email);
+    if (!user) {
+      return res.json({ errorCode: true, data: "This account is not exist" });
+    }
+    const newpass = Math.floor(Math.random() * 10000) + 1000;
+    data.password = bcrypt.hash(newpass, saltRounds);
+    const update = await userCol.update(user.code, data);
+    if (!update) {
+      return res.json({ errorCode: true, data: "System error" });
+    }
+    for (property of userCol.userProperties) {
+      if (req.body[property]) {
+        update[property] = req.body[property];
+      }
+    }
+    sendPass(data.email, newpass);
+    return res.json({ errorCode: false, data: update });
+  } catch (error) {
+    return res.json({ errorCode: true, data: "system error" });
   }
-  sendPass(data.email, newpass);
-  return res.json({ errorCode: false, data: update });
 }
 
 async function userAuthentication(req, res, next) {
